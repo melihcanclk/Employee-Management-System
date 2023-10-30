ee60d6c03d42e4981bcd2a40018cd77fbba1b12a
Melihcan Cilek <melihcanclk@hotmail.com>
2023-10-30 13:46:58 +0300

    fix(frontend): dark theme in class level

--- employee-management-system-frontend/src/components/Header/index.tsx
+++ employee-management-system-frontend/src/components/Header/index.tsx
@@ -42,8 +42,8 @@ const Header = () => {
         <>
             <header
                 className={`header left-0 top-0 z-40 flex w-full items-center ${sticky
-                        ? "dark:bg-gray-dark dark:shadow-sticky-dark fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
-                        : "absolute bg-transparent"
+                    ? "dark:bg-gray-dark dark:shadow-sticky-dark fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
+                    : "absolute bg-transparent"
                     }`}
             >
                 <div className="container">
@@ -94,8 +94,8 @@ const Header = () => {
                                 <nav
                                     id="navbarCollapse"
                                     className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${navbarOpen
-                                            ? "visibility top-full opacity-100"
-                                            : "invisible top-[120%] opacity-0"
+                                        ? "visibility top-full opacity-100"
+                                        : "invisible top-[120%] opacity-0"
                                         }`}
                                 >
                                     <ul className="block lg:flex lg:space-x-12">
@@ -105,8 +105,8 @@ const Header = () => {
                                                     <Link
                                                         href={menuItem.path}
                                                         className={`flex py-2 text-base lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${usePathName === menuItem.path
-                                                                ? "text-primary dark:text-white"
-                                                                : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
+                                                            ? "text-primary dark:text-white"
+                                                            : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
                                                             }`}
                                                     >
                                                         {menuItem.title}
@@ -153,7 +153,7 @@ const Header = () => {
                             <div className="flex items-center justify-end pr-16 lg:pr-0">
                                 <Link
                                     href="/signin"
-                                    className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block"
+                                    className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70  dark:text-white md:block"
                                 >
                                     Sign In
                                 </Link>
--- employee-management-system-frontend/tailwind.config.ts
+++ employee-management-system-frontend/tailwind.config.ts
@@ -6,12 +6,12 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
       padding: "1rem",
     },
-
     screens: {
       xs: "450px",
       // => @media (min-width: 450px) { ... }
